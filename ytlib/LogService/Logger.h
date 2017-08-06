1e6cf44857f9a83d3e426754513827115c6af1b8
WTDESKTOP <905976782@qq.com>
2017-08-06 23:19:57 +0800

    log模块

--- ytlib/LogService/Logger.h
+++ ytlib/LogService/Logger.h
@@ -1,9 +1,114 @@
 #pragma once
+#include <ytlib/NetTools/TcpNetUtil.h>
+#include <ytlib/Common/Util.h>
+#include <ytlib/LogService/LoggerServer.h>
 
-//todoï¼š ä½¿ç”¨boost.logåº“ã€ç½‘ç»œé€‚é…å™¨ã€ç®€å•æ•°æ®åº“æ¥å®Œæˆä¸€ä¸ªæ—¥å¿—æœåŠ¡å™¨
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
+
+
 
 namespace ytlib {
+	//Í¬²½ºó¶Ë£¬²»ĞèÒª¼ÓËø
+	class NetBackend : public boost::log::sinks::basic_formatted_sink_backend<
+		char,boost::log::sinks::synchronized_feeding> {
+	public:
+		//Ä¿Ç°Ö»Ö§³ÖintĞÍid¡£Èç¹ûÒª¸Ä³ÉstringĞÍidÒ²ºÜ¼òµ¥
+		explicit NetBackend(uint32_t myid_,const TcpEp& logserver_ep_ ):
+			sock(service), LogServerEp(logserver_ep_), ConnectFlag(false){
+			header[0] = LogConnection::TCPHEAD1;
+			header[1] = LogConnection::TCPHEAD2;
+			header[2] = LogConnection::LOGHEAD1;
+			header[3] = LogConnection::LOGHEAD2;
+			logBuff.push_back(std::move(boost::asio::const_buffer(header, HEAD_SIZE)));
+			//ÉèÖÃ±¾»úid¡£Èç¹ûÒÔºóÒªÌí¼Ó±¾µØĞÅÏ¢Ò²ÔÚ´Ë´¦Ìí¼ÓÍØÕ¹
+			HostInfoSize = 4;
+			HostInfoBuff = boost::shared_array<char>(new char[HostInfoSize]);
+			set_buf_from_num(HostInfoBuff.get(), myid_);
+			logBuff.push_back(std::move(boost::asio::const_buffer(HostInfoBuff.get(), HostInfoSize)));
+			connect();
+		}
+		void consume(boost::log::record_view const& rec, std::string const& command_line) {
+			//Èç¹ûÓĞÁ¬½Ó²Å·¢ËÍ£¬·ñÔò²»·¢ËÍ
+			if (connect()) {
+				set_buf_from_num(&header[4], static_cast<uint32_t>(command_line.size() + HostInfoSize));
+				logBuff.push_back(std::move(boost::asio::buffer(command_line)));
+				boost::system::error_code err;
+				//·¢ËÍÊ§°ÜÔò½«ConnectFlagÖÃÎªfalse
+				sock.write_some(logBuff, err);
+				logBuff.pop_back();
+				if (err) {
+					ConnectFlag = false;
+					YT_DEBUG_PRINTF("send to log server failed : %s\n", err.message().c_str());
+					return;
+				}
+			}
+		}
+	private:
+		bool connect() {
+			if (ConnectFlag) return true;
+			sock.open(boost::asio::ip::tcp::v4());
+			boost::system::error_code err;
+			sock.connect(LogServerEp, err);
+			if (err) {
+				YT_DEBUG_PRINTF("connect to log server failed : %s\n", err.message().c_str());
+				return false;
+			}
+			ConnectFlag = true;
+			return true;
+		}
 
+		boost::asio::io_service service;//È«Í¬²½²Ù×÷£¬ËùÒÔ²»ĞèÒªrun
+		TcpSocket sock;
+		TcpEp LogServerEp;
+		std::atomic_bool ConnectFlag;
+		std::vector<boost::asio::const_buffer> logBuff;
+		boost::shared_array<char> HostInfoBuff;
+		uint32_t HostInfoSize;
+		static const uint8_t HEAD_SIZE = 8;
+		char header[HEAD_SIZE];//±¨Í·»º´æ
 
-	
+	};
+
+	//ÈÕÖ¾¿ØÖÆÖĞĞÄ¡£Ìá¹©È«¾Öµ¥Àı¡£Ä¬ÈÏÊ¹ÓÃ¿ØÖÆÌ¨ÈÕÖ¾¡£ĞèÒªÊÖ¶¯ÉèÖÃÍøÂçÈÕÖ¾
+	class LogControlCenter {
+	public:
+		LogControlCenter() {
+
+		}
+		~LogControlCenter() {
+
+		}
+		void EnableConsoleLog() {
+
+		}
+		void DisableNetLog() {
+
+		}
+		void EnableNetLog() {
+
+		}
+		void DisableConsoleLog() {
+
+		}
+		
+
+	private:
+
+	};
+
+	//Ò»Ğ©ºê¶¨Òå
+#define YT_LOG_TRACE 
+#define YT_LOG_DEBUG
+#define YT_LOG_INFO
+#define YT_LOG_WARNING
+#define YT_LOG_ERROR
+#define YT_LOG_FATAL
+
 }
--- ytlib/Test/main.cpp
+++ ytlib/Test/main.cpp
@@ -3,6 +3,7 @@
 #include <ytlib/SupportTools/UUID.h>
 #include <ytlib/NetTools/TcpNetAdapter.h>
 #include <ytlib/LogService/LoggerServer.h>
+#include <ytlib/LogService/Logger.h>
 
 #include "mathtest.h"
 #include "filetest.h"
@@ -13,6 +14,13 @@
 
 using namespace std;
 using namespace ytlib;
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace keywords = boost::log::keywords;
+namespace expr = boost::log::expressions;
+//
+//BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(my_logger, src::logger_mt)
 
 int32_t main(int32_t argc, char** argv) {
 	YT_DEBUG_PRINTF("-------------------start-------------------\n");
@@ -27,6 +35,30 @@ int32_t main(int32_t argc, char** argv) {
 
 	//tocTime_global = boost::posix_time::microsec_clock::universal_time(); 
 	//std::cout << (tocTime_global - ticTime_global).ticks() << "us" << std::endl;
+
+	
+
+	//src::logger_mt& lg = my_logger::get();
+	//BOOST_LOG(lg) << "Greetings from the global logger!";
+
+	boost::shared_ptr< logging::core > core = logging::core::get();
+
+	typedef boost::log::sinks::synchronous_sink< NetBackend > sink_t;
+	
+	boost::shared_ptr< sink_t > sink(new sink_t());
+
+	sink->set_formatter(
+		expr::stream<<"["
+		<< expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S")
+		<< "]: <" << logging::trivial::severity
+		<< "> " << expr::smessage
+	);
+	core->add_sink(sink);
+	core->remove_sink(sink);
+	logging::add_common_attributes();
+
+	BOOST_LOG_TRIVIAL(debug) << "Hello, it's a simple notification";
+
 
 	test_TcpNetAdapter();
 	
