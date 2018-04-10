06a2aa9eff11ba63a4c10de5b54b38aed6458a6a
WTDESKTOP <905976782@qq.com>
2018-04-10 20:08:10 +0800

    Signed-off-by: WTDESKTOP <905976782@qq.com>

--- ytlib/LightMath/Mathbase.h
+++ ytlib/LightMath/Mathbase.h
@@ -2,6 +2,7 @@
 #include <ytlib/Common/Util.h>
 #include <cmath>
 #include <vector>
+#include <map>
 
 #define USE_DOUBLE_PRECISION
 
@@ -22,7 +23,7 @@ typedef float  tfloat;    // single precision
 namespace ytlib
 {
 	//计算有多少个1
-	static uint32_t count_1_(uint32_t n) {
+	static uint32_t count_1_(uint64_t n) {
 		uint32_t num = 0;
 		while (n) {
 			n &= (n - 1);
@@ -30,23 +31,32 @@ namespace ytlib
 		}
 		return num;
 	}
-
 	//计算有多少个0
-	static uint32_t count_0_(uint32_t n) {
+	static uint32_t count_0_(uint64_t n) {
 		return count_1_(~n);
 	}
 
+	//计算在pos之前的第一个1的位置
+	static uint32_t find_1_(uint64_t val, uint32_t pos) {
+		assert(pos < 64);
+		if (!val) return 64;
+		while (0 == (val & (1 << pos))) {
+			++pos;
+		}
+		return pos;
+	}
+
 	//判断是否是质数
-	static bool isPrime(uint32_t num) {
+	static bool isPrime(uint64_t num) {
 		//两个较小数另外处理  
 		if (num == 2 || num == 3)
 			return true;
 		//不在6的倍数两侧的一定不是质数  
 		if (num % 6 != 1 && num % 6 != 5)
 			return false;
-		uint32_t tmp = uint32_t(std::sqrt(num));
+		uint64_t tmp = uint64_t(std::sqrt(num));
 		//在6的倍数两侧的也可能不是质数  
-		for (uint32_t i = 5; i <= tmp; i += 6)
+		for (uint64_t i = 5; i <= tmp; i += 6)
 			if (num %i == 0 || num % (i + 2) == 0)
 				return false;
 		//排除所有，剩余的是质数  
@@ -54,7 +64,7 @@ namespace ytlib
 
 	}
 	//求最大公约数
-	static uint32_t gcd(uint32_t num1, uint32_t num2) {
+	static uint64_t gcd(uint64_t num1, uint64_t num2) {
 		if (num1 < num2) return gcd(num2, num1);
 		if (num2 == 0) return num1;
 		if (num1 & 1) {
@@ -68,15 +78,26 @@ namespace ytlib
 		}
 	}
 	//求最小公倍数：num1*num2/gcd(num1,num2)
-	static uint32_t lcm(uint32_t num1, uint32_t num2) {
+	static uint64_t lcm(uint64_t num1, uint64_t num2) {
 		return num1 / gcd(num1, num2) * num2;
 	}
 	//分解质因数
-	static void factoring(uint32_t num, std::vector<uint32_t>& re) {
+	static void factoring(uint64_t num, std::vector<uint64_t>& re) {
 		if (num < 2) return;
-		for (uint32_t ii = 2; ii <= num; ++ii) {
+		for (uint64_t ii = 2; ii <= num; ++ii) {
 			while (num%ii == 0) {
 				re.push_back(ii);
+				num /= ii;
+			}
+		}
+	}
+	static void factoring(uint64_t num, std::map<uint64_t, uint64_t>& re) {
+		if (num < 2) return;
+		for (uint64_t ii = 2; ii <= num; ++ii) {
+			while (num%ii == 0) {
+				std::map<uint64_t, uint64_t>::iterator itr = re.find(ii);
+				if (itr == re.end()) re.insert(std::pair<uint64_t, uint64_t>(ii, 1));
+				else ++(itr->second);
 				num /= ii;
 			}
 		}
@@ -128,5 +149,15 @@ namespace ytlib
 		return (q == 1.0) ? (n*a1) : (a1*(1 - std::pow(q, n)) / (1 - q));
 	}
 
+	// pow
+	static uint64_t pow(const uint64_t &value, uint64_t n) {
+		uint64_t tmp = value, re = 1;
+		for (; n; n >>= 1) {
+			if (n & 1)
+				re *= tmp;
+			tmp *= tmp;
+		}
+		return re;
+	}
 
 }
